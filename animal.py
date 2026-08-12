e17f6d12b300d673d05f4493770d22dc5c257e10
jmjoy46-star <jmjoy46@gmail.com>
2026-08-12 09:44:01 +0530

    Initial Commit

--- animal.py
+++ animal.py
@@ -1,6 +1,7 @@
 class Animal:
     def character(self):
         print("The animal is mammal")
+        print("animal is beautiful")
         print("animal is live in forest")
 class Mammal(Animal):
     def food(self):
